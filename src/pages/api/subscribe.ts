96b2e9f61c36eb8e83fcb1ff4cb9c158d5b11eca
Eduardo J. Guerra <edujguerra@gmail.com>
2022-11-16 21:59:25 -0300

    2211162159

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import { stripe } from "../../services/stripe";
-
+    
+        
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method=== 'POST'){
         const session = await getSession({req})
-
+        //const user = await req.body.user;
+        
         const stripeCustomer = await stripe.customers.create({
             email: session?.user?.email!,
         })
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -4,7 +4,6 @@ import { version } from '../../package.json'
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
     apiVersion: '2022-08-01',
     appInfo: {
-      name: 'Ignews',
-      version: '0.1.0',
+      name: 'Ignews',      
     },
   });
